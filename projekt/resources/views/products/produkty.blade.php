52776ce8b1b770d444bad31ee2a4cee9cf8f02c2
flor3kk <szkolny.7883@gmail.com>
2024-04-21 20:13:52 +0200

    szablon

---
+++ projekt/resources/views/products/produkty.blade.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="description" content="Prodajemo bombone, čokolade i sladolede! Kliknite kako biste mogli da odaberete šta vas najviše zanima!" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="Slatkisi, Cokolade, Bombone, Sladoledi, Online prodavnica, Poslasticarnica, Slatko">
+    <meta name="author" content="Filip Babić, rookybusiness@gmail.com">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Iconica i ime taba -->
+    <link rel="icon" href="img/logo-icon.png">
+    <title>CandyShop | Proizvodi</title>
+  </head>
+  <body>
+
+    <!-- Navigation Bar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-danger">
+      <a class="navbar-brand" href="index.html"><img src="img/logo-small.png" class="img-fluid" alt="CandyShop"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Početna <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="onama.html">O nama</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="proizvodi.html">Proizvodi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="galerija.html">Galerija</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="kontakt.html">Kontakt</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div id="wrap">
+      <div id="main" class="container-fluid clear-top" style="max-width: max-content;">
+
+        <div class="row justify-content-center">
+          <div class="col-12 col-lg-4">
+            <div class="card shadow" style="min-height: 850px;">
+              <img src="img/proizvodi_bombone.jpg" class="card-img-top razmak" alt="bombone">
+              <div class="card-body text-center">
+                <h1 class="display-3">Bombone</h1>
+                <p class="card-text">Proizvedene u Srbiji, domaća proizvodnja sa prirodnim i kvalitetnim proizvodima.
+                  <br>U ponudi imamo veliki asortiman različitih ukusa, vrsta i boja za sve prilike!</p>
+                  <ul class="list-unstyled">
+                    <li>Od vrsta bombona nudimo:</li>
+                    <li>Tvrde bombone</li>
+                    <li>Gumene bombone</li>
+                    <li>Sladiće</li>
+                    <li>Žvake</li>
+                  </ul>
+                <a href="bombone.html" class="btn btn-danger">Lista Proizvoda</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4">
+            <div class="card shadow" style="min-height: 850px;">
+              <img src="img/proizvodi_cokolade.jpg" class="card-img-top razmak" alt="čokolade">
+              <div class="card-body text-center">
+                <h1 class="display-3">Čokolade</h1>
+                <p class="card-text">Za naše drage potrošače samo najkvalitetniji i najnežniji čokoladni proizvodi!
+                  <br>Kakao uvezen iz Perua a prerađen u naše drage poslastice u Srbiji.
+                  <br>Nudimo izbor između više vrsta čokolada kao i opciju pravljenja specijalnih porudžbina po potrebi (za više informacija kontaktirajte nas preko <a href="kontakt.html" class="text-danger">kontakt stranice</a>)</p>
+                  <ul class="list-unstyled">
+                    <li>Od vrsta čokolada nudimo (više vrsta):</li>
+                    <li>Mlečne čokolade</li>
+                    <li>Crne čokolade za kuvanje</li>
+                    <li>Bele čokolade</li>
+                  </ul>
+                <a href="cokolade.html" class="btn btn-danger">Lista Proizvoda</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4">
+            <div class="card shadow" style="min-height: 850px;">
+              <img src="img/proizvodi_sladolei.jpg" class="card-img-top razmak" alt="Sladoledi">
+              <div class="card-body text-center">
+                <h1 class="display-3">Sladoledi</h1>
+                <p class="card-text">Sladoledi su najnovija adicija u našem asortimanu. Naši ukusi su jaki, a tekstura kremasta.
+                  <br>Kao i sve ostalo, naši sladoledi us takođe napravljeni od prirodnih proizvoda što poboljšava ukus i proizvedeni su u Srbiji!
+                  Za sada nudimo 12 različitih ukusa i planiramo da proširimo ovaj broj.</p>
+                  <ul class="list-unstyled">
+                    <li>Od ukusa, najpopularniji su:</li>
+                    <li>Vaniličansvo</li>
+                    <li>Čokoladni Mix</li>
+                    <li>Bobičasto Uživanje</li>
+                  </ul>
+                <a href="sladoledi.html" class="btn btn-danger">Lista Proizvoda</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- Footer stranice -->
+    <footer>
+      <div class="row text-center" style="margin: 0 !important;">
+        <div class="col-12">
+          <ul class="list-inline">
+            <li class="list-inline-item"><a href="https://www.facebook.com/candydotcom" target="_blank"><i class="fab fa-facebook"></i></a></li>
+            <li class="list-inline-item"><a href="https://www.instagram.com/candydotcom/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="https://twitter.com/candydotcom" target="_blank"><i class="fab fa-twitter"></i></a></li>
+          </ul>
+          <ul class="list-inline">
+            <li class="list-inline-item"><a href="faq.html">Česta pitanja</a></li>
+            <li class="list-inline-item"><a href="cenovnik.html">Cenovnik</a></li>
+            <li class="list-inline-item"><a href="mapa.html">Mapa sajta</a></li>
+            <li class="list-inline-item"><a href="kontakt.html">Kontakt</a></li>
+          </ul>
+          <p>Copyright <i class="far fa-copyright"></i> 2020, CandyShop All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <!-- FontAwesome Plug -->
+    <script src="https://kit.fontawesome.com/6b3d427563.js" crossorigin="anonymous"></script>
+  </body>
+</html>
