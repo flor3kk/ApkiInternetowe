bcfbe32e1ed9aea1aeb64ce467c4055ab854295c
florek <mail@123>
2024-06-15 14:43:27 +0200

    ?

--- projekt/resources/views/shared/navbar.blade.php
+++ projekt/resources/views/shared/navbar.blade.php
@@ -1,6 +1,6 @@
     <!-- Pasek nawigacyjny -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-danger">
-        <a class="navbar-brand" href="welcome"><img src="img\logo.png" class="img-fluid" alt="CandyShop"></a>
+        <a class="navbar-brand" href="{{ url('welcome') }}"><img src="img\logo.png" class="img-fluid" alt="CandyShop"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
